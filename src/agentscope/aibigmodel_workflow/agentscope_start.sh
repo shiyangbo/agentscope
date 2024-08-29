dd5f58f888c0c32ac141b0a994914b5a31f61dbe
liugeyi <liugy177@chinaunicom.cn>
2024-08-29 09:59:51 +0800

    修改连接池重连时间

--- src/agentscope/aibigmodel_workflow/agentscope_start.sh
+++ src/agentscope/aibigmodel_workflow/agentscope_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+rm -rf /ainfs_huhe_dev/agentscope_code/agentscope/src/agentscope/aibigmodel_workflow/ sql_config.yaml
+cp -r /ainfs_huhe_dev/agentscope_code/sql_config.yaml /ainfs_huhe_dev/agentscope_code/agentscope/src/agentscope/aibigmodel_workflow/
 container_id=$(docker ps -a --filter "name=agentscope_workflow" -q)
 
 if [ -z "$container_id" ]; then
@@ -11,3 +12,7 @@ else
 fi
 
 docker run -tid --restart=always  -p 6671:6671 --net=kong-net --name agentscope_workflow -v /ainfs_huhe_dev/agentscope_code:/agentscope --init agentscope_workflow:v2 python3 /agentscope/agentscope/src/agentscope/aibigmodel_workflow/app.py
+
+sleep 2
+container_id=$(docker ps -a --filter "name=agentscope_workflow" -q)
+docker logs $container_id
