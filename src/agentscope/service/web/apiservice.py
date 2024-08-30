f278434bdc22edaea823681f7d99c40ae1c5f54b
shiyangbo <271988813@qq.com>
2024-08-30 11:48:49 +0800

    增加api超时阈值

--- src/agentscope/service/web/apiservice.py
+++ src/agentscope/service/web/apiservice.py
@@ -57,6 +57,7 @@ def api_request(
             data=data,
             json=json,
             headers=headers,
+            timeout=30,  # 简单设置最大超时阈值成30秒
             **kwargs,
         )
         resp.raise_for_status()  # Raise an error for bad responses
