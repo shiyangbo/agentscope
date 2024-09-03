1b4b5ff8c5cdea6564013b9287a755a0a7f00a24
shiyangbo <271988813@qq.com>
2024-09-03 10:43:55 +0800

    add test init

--- src/agentscope/aibigmodel_workflow/app.py
+++ src/agentscope/aibigmodel_workflow/app.py
@@ -35,32 +35,44 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
 
-# 读取 YAML 文件
-with open('/agentscope/agentscope/src/agentscope/aibigmodel_workflow/sql_config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
-
-# 从 YAML 文件中提取参数
-DIALECT = config['DIALECT']
-DRIVER = config['DRIVER']
-USERNAME = config['USERNAME']
-PASSWORD = config['PASSWORD']
-HOST = config['HOST']
-PORT = config['PORT']
-DATABASE = config['DATABASE']
-SERVICE_URL = config['SERVICE_URL']
-
-SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT,
-                                                                       DATABASE)
-print(SQLALCHEMY_DATABASE_URI)
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_ECHO'] = True
-app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
-    'pool_size': 5,
-    'pool_timeout': 30,
-    'pool_recycle': -1,
-    'pool_pre_ping': True
-}
+
+# 读取 YAML 文件
+test_without_mysql = True
+from agentscope.utils.tools import _is_windows
+if _is_windows and test_without_mysql:
+    # Set the cache directory
+    from pathlib import Path
+    _cache_dir = Path.home() / ".cache" / "agentscope-studio"
+    _cache_db = _cache_dir / "agentscope.db"
+    os.makedirs(str(_cache_dir), exist_ok=True)
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{str(_cache_db)}"
+else:
+    with open('/agentscope/agentscope/src/agentscope/aibigmodel_workflow/sql_config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+
+    # 从 YAML 文件中提取参数
+    DIALECT = config['DIALECT']
+    DRIVER = config['DRIVER']
+    USERNAME = config['USERNAME']
+    PASSWORD = config['PASSWORD']
+    HOST = config['HOST']
+    PORT = config['PORT']
+    DATABASE = config['DATABASE']
+    SERVICE_URL = config['SERVICE_URL']
+
+    SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST,
+                                                                           PORT,
+                                                                           DATABASE)
+    print(SQLALCHEMY_DATABASE_URI)
+    app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+        'pool_size': 5,
+        'pool_timeout': 30,
+        'pool_recycle': -1,
+        'pool_pre_ping': True
+    }
 
 db = SQLAlchemy()
 db.init_app(app)
